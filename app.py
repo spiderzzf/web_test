9d90fe8743b13bacdff82c14fb87b09233d04756
spiderzzf <innozzf@123>
2025-03-07 14:43:08 +0800

    1

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)  # 在当前文件下创建应用
+
+@app.route("/test")
+def test():
+
+    return {"Hello": "World"}
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8062,threaded=False)  # 运行app
+
